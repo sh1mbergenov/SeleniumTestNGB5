e5964f8e2970440e0a0516d90a5f1b77f451ea81
sh1mbergenov <shimbergenov01@gmail.com>
2024-05-01 11:27:42 -0500

    added a new class

---
+++ src/test/java/tests/ConflictTests.java
@@ -0,0 +1,19 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ConflictTests {
+
+    @Test
+    public void test1(){
+        int a = 5;
+        int b = 10;
+        System.out.println("Sum : " + (a+b));
+    }
+    @Test
+    public void test2(){
+       String message = "Welcome to USA";
+        Assert.assertTrue(message.contains("USA"));
+    }
+}
