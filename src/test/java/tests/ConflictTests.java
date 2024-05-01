2dfb447b99f599a65c6e1d94bdd91950a16ec067
sh1mbergenov <77388053+sh1mbergenov@users.noreply.github.com>
2024-05-01 12:57:53 -0500

    Update ConflictTests.java

--- src/test/java/tests/ConflictTests.java
+++ src/test/java/tests/ConflictTests.java
@@ -15,7 +15,7 @@ public class ConflictTests {
     @Test
     public void test2(){
        String message = "Welcome to USA - the land of opportunity";
-        Assert.assertTrue(message.toLowerCase().contains("usa"));
+        Assert.assertTrue(message.toLowerCase().contains("land"));
     }
 
 
