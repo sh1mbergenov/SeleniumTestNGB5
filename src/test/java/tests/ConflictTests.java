d824bbbb8a35c97c33cb38d39e7584dc9286a68d
sh1mbergenov <shimbergenov01@gmail.com>
2024-05-01 11:47:40 -0500

    modified test2 method

--- src/test/java/tests/ConflictTests.java
+++ src/test/java/tests/ConflictTests.java
@@ -13,7 +13,7 @@ public class ConflictTests {
     }
     @Test
     public void test2(){
-       String message = "Welcome to USA";
+       String message = "Welcome to USA - the land of opportunity";
         Assert.assertTrue(message.toLowerCase().contains("usa"));
     }
     @Test
